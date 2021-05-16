cda9ee8841bdf2f487b9d272b79e7e6051a8489e
k_yamanaka <vaivailx+git@gmail.com>
2021-05-16 21:16:00 +0900

    勝敗判定をするクラスを追加する

--- package/ox_game.py
+++ package/ox_game.py
@@ -1,3 +1,6 @@
+from itertools import chain
+
+
 class Board:
     MARU = "o"
     BATSU = "x"
@@ -23,19 +26,70 @@ class BoardViewer:
         self._board = board
 
     def print(self):
-        for rows in self._board.cells:
-            for cell in rows:
+        for row in self._board.cells:
+            for cell in row:
                 print("|" + cell, end="")
             else:
                 print("|")
 
 
+class Referee:
+    def __init__(self, board: Board) -> None:
+        self._turn_count = 0
+        self._board = board
+
+    @property
+    def turn_count(self):
+        return self._turn_count
+
+    @turn_count.setter
+    def turn_count(self):
+        self._turn_count = self._turn_count + 1
+
+    def judge_victory(self):
+        cells = self._board.cells
+        lines = [
+            # horizontal
+            [cells[0][0], cells[0][1], cells[0][2]],
+            [cells[1][0], cells[1][1], cells[1][2]],
+            [cells[2][0], cells[2][1], cells[2][2]],
+            # verticals
+            [cells[0][0], cells[1][0], cells[2][0]],
+            [cells[0][1], cells[1][1], cells[2][1]],
+            [cells[0][2], cells[1][2], cells[2][2]],
+            # diagonals
+            [cells[0][0], cells[1][1], cells[2][2]],
+            [cells[2][0], cells[1][1], cells[0][2]],
+        ]
+        for line in lines:
+            not_empties = list(
+                filter(lambda cell: cell in [Board.MARU, Board.BATSU], line)
+            )
+            if len(not_empties) == 3 and len(set(not_empties)) == 1:
+                return not_empties.pop()
+        return None
+
+
 def main():
     board = Board()
     viewer = BoardViewer(board)
+    referee = Referee(board)
     viewer.print()
-    board.update_cell(0, 1, Board.MARU)
+    board.update_cell(0, 0, Board.MARU)
     viewer.print()
+    print(referee.judge_victory())
+    board.update_cell(0, 2, Board.BATSU)
+    viewer.print()
+    print(referee.judge_victory())
+    board.update_cell(1, 1, Board.MARU)
+    viewer.print()
+    print(referee.judge_victory())
+    board.update_cell(1, 2, Board.BATSU)
+    viewer.print()
+    print(referee.judge_victory())
+    board.update_cell(2, 2, Board.MARU)
+    viewer.print()
+    print(referee.judge_victory())
 
 
 if __name__ == "__main__":
